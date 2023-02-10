1d14e86a071cfcb1960b66ea6594728768976f63
Mykola Kvach <mykola_kvach@epam.com>
2023-02-10 10:01:12 +0200

    soc: arm64: add Renesas Rcar Gen3 SoC support
    Added files for supporting  arm64 Renesas r8a7795 SoC:
        * add dtsi for r8a7795 soc;
        * files for support pfc.
    
    Note: some of files copy-pasted from Renesas Arm 32
          SoC directory and this commit is a temporary
          solution, we need to rework it in order to use
          the same source files for both architectures.
    
    Enable build of clock_control_r8a7795_cpg_mssr.c for
    a new ARM64 SoC R8A7795.
    
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>

---
+++ soc/arm64/renesas_rcar/gen3/soc.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2023 EPAM Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#include <zephyr/arch/cpu.h>
+#include <zephyr/device.h>
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+#include <soc.h>
+
+/**
+ *
+ * @brief Perform basic hardware initialization
+ *
+ * @return 0
+ */
+
+static int soc_init(void)
+{
+	/* Install default handler that simply resets the CPU
+	 * if configured in the kernel, NOP otherwise
+	 */
+	return 0;
+}
+
+SYS_INIT(soc_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
