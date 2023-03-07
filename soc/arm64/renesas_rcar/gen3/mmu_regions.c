bd4963167fb3d7d2f9d582ef8a4bb0d36886b6c9
Mykola Kvach <mykola_kvach@epam.com>
2023-03-07 18:23:28 +0200

    soc: arm64: add MMC node to dtsi
        - added MMC node to R8A7795 SoC;
        - added mapping of MMC device in case when MMC node has status "okay".
    
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>

--- soc/arm64/renesas_rcar/gen3/mmu_regions.c
+++ soc/arm64/renesas_rcar/gen3/mmu_regions.c
@@ -7,6 +7,9 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/sys/util.h>
 
+#define ROUND_UP_SIZE_MMU_PAGE_FOR_NODE(inst, node) \
+	ROUND_UP(DT_REG_SIZE(DT_INST(inst, node)), CONFIG_MMU_PAGE_SIZE)
+
 static const struct arm_mmu_region mmu_regions[] = {
 	MMU_REGION_FLAT_ENTRY("GIC",
 			      DT_REG_ADDR_BY_IDX(DT_INST(0, arm_gic), 0),
@@ -29,6 +32,12 @@ static const struct arm_mmu_region mmu_regions[] = {
 			      DT_REG_ADDR(DT_INST(0, renesas_r8a7795_cpg_mssr)),
 			      DT_REG_SIZE(DT_INST(0, renesas_r8a7795_cpg_mssr)),
 			      MT_DEVICE_nGnRnE | MT_RW | MT_NS),
+#if DT_NODE_HAS_STATUS(DT_INST(0, renesas_rcar_mmc), okay)
+	MMU_REGION_FLAT_ENTRY("MMC",
+			      DT_REG_ADDR(DT_INST(0, renesas_rcar_mmc)),
+			      ROUND_UP_SIZE_MMU_PAGE_FOR_NODE(0, renesas_rcar_mmc),
+			      MT_DEVICE_nGnRnE | MT_RW | MT_NS),
+#endif
 
 #if DT_NODE_HAS_STATUS(DT_INST(0, renesas_rcar_pfc), okay)
 	MMU_REGION_FLAT_ENTRY("PFC",
